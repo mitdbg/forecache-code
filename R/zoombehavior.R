bb5a22c186c2f5ec25f1c2c3c2259c5967066b10
Leilani Battle <leibatt@gmail.com>
2015-02-28 21:54:09 -0500

    should be done addressing feedback

--- R/zoombehavior.R
+++ R/zoombehavior.R
@@ -1,5 +1,7 @@
 library(ggplot2)
 
+theme_set(theme_grey(base_size = 16)) 
+
 datapath = "/Volumes/E/mit/vis/code/scalar-prefetch/results/zoom_info.csv"
 figpath = "/Volumes/E/mit/vis/code/scalar-prefetch/code/R/move_analysis/"
 tasknames = c("task1","task2","task3")
@@ -10,7 +12,7 @@ d <- data.frame(rawdata)
 ids <- unique(d$id) # get unique user id's
 
 for(i in ids) {
-#for(i in c(121,123)) {
+#for(i in c(123)) {
   cat(paste("retrieving data for user ",i,"\n",sep=""))
 for(tid in taskids) {
   tn = tasknames[tid]
@@ -24,8 +26,11 @@ for(tid in taskids) {
   #p <- ggplot(d2,aes(x=x,y=y,group=taskname,color=factor(taskname)))
   p <- p + geom_line()
   p<- p + geom_point() + scale_y_reverse() + xlab("Tile Request ID") + ylab("Zoom Level")
-  p <- p + ggtitle(paste("Changes in Zoom Level Over Time for User ",i," and Task ",tid,sep=""))
+  p <- p + ggtitle(paste("Change in Zoom Level\nOver Time (User ",i,", Task ",tid,")",sep=""))
+  #p <- p + ggtitle(paste("Change in Zoom Level\nOver Time (User 2, Task ",tid,")",sep=""))
   print(p)
-  ggsave(paste(figpath,tn,"/user",i,"_",tn,".png",sep=""),width=7,height=4)
+  ggsave(paste(figpath,tn,"/user",i,"_",tn,".png",sep=""),width=6,height=4)
+  #ggsave(paste(figpath,tn,"/user2_",tn,".png",sep=""),width=6,height=4)
 }
 }
+#}
