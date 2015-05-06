e89c24f1bc33bf285d2963039982e6c5726354ca
Leilani Battle <leibatt@gmail.com>
2015-05-05 20:13:31 -0400

    now we actually measure network costs

--- harness/src/backend/MainThread.java
+++ harness/src/backend/MainThread.java
@@ -579,7 +579,7 @@ public class MainThread {
 				//long s = System.currentTimeMillis();
 				byte[] toSend = NiceTilePacker.packNiceTile(t);
 				//long e = System.currentTimeMillis();
-				response.getWriter().println(toSend);
+				response.getWriter().println(new String(toSend));
 				//long e2 = System.currentTimeMillis();
 				//String report= (e-s)+","+(e2-e)+","+toSend.length;
 				//System.out.println(report);
